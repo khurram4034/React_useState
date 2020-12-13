4c4b1f8dcdc2add2463068ade33b6ae6a4682eb7
khurram4034 <64483246+khurram4034@users.noreply.github.com>
2020-12-13 19:40:43 +0500

    useState code is updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import './App.css';
-import Co from "./Co";
 import CounterState from "./CounterState";
 import Day from "./Day";
 import Message from "./Message";
@@ -8,7 +7,6 @@ import Room from "./Room";
 
 function App() {
   let [number, setNumber] = useState(1);
-  let [num, setnum] = useState(1)
  
   return (
     <div className="App">
@@ -20,8 +18,7 @@ function App() {
       <Day />
       <br />
       <Room />
-      <Co number={num} />
-      <button onClick={()=>setnum(num + 1)}>Number Updated</button>
+
     </div>
   );
 }
